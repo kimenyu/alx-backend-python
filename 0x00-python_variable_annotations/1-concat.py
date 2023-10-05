4f4026ba82413cd865f088781691326e9a7c9831
kimenyu <njorogekimenyu@gmail.com>
2023-10-05 10:03:04 +0000

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""string antonations
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat two strings
+
+    Args:
+        str1 (str): arg 1 as a string antonation 
+        str2 (str): arg 1 as a string antonation
+
+    Returns:
+        str: concatinates str1 and str2 and retturs a string
+    """
+    return str1 + str2
