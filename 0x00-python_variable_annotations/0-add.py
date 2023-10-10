8a68544904d8072355061561894b6387e53ba693
kimenyu <njorogekimenyu@gmail.com>
2023-10-10 13:46:10 +0000

    rem

--- 0x00-python_variable_annotations/0-add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""This is the typing module.
-This module provides runtime support for type hints."""
-
-
-def add(a: float, b: float) -> float:
-    """add two floats"""
-    return a + b
