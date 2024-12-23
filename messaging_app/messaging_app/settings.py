10bd870c15231658e834721f4a3cf4dd17861848
kimenyu <njorogekimenyu@gmail.com>
2024-12-23 06:41:49 +0300

    adding initial message app config

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -82,6 +82,17 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated', 
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication', 
+    ],
+}
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
