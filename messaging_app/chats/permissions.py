b27145b8884cde8d1bed63721d1e79c87ad2fd8d
kimenyu <njorogekimenyu@gmail.com>
2025-01-20 19:30:03 +0300

    making changes

--- messaging_app/chats/permissions.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import permissions
-
-class IsParticipantOfConversation(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return request.user in obj.participants.all()
